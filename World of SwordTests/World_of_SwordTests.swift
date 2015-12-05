d0d2e3a6946b6ac98479c094a7ed4cac5b906710
Mark Robuck <markrobuck@ryans-macbook-pro.local>
2015-12-05 09:18:21 -0500

    Initial Commit

---
+++ World of SwordTests/World_of_SwordTests.swift
@@ -0,0 +1,36 @@
+//
+//  World_of_SwordTests.swift
+//  World of SwordTests
+//
+//  Created by Mark Robuck on 12/5/15.
+//  Copyright © 2015 Robuck Inc. All rights reserved.
+//
+
+import XCTest
+@testable import World_of_Sword
+
+class World_of_SwordTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
